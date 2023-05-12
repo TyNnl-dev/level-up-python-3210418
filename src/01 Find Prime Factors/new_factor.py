73575d4cea4bd8c9f36a8d8e7c83887ec89768ac
Lam Cao Tho Trinh <BNWT43@motorolasolutions.com>
2023-05-12 16:24:25 +0700

    Add final result for Find Prime Factors ex

--- src/01 Find Prime Factors/new_factor.py
+++ src/01 Find Prime Factors/new_factor.py
@@ -1,3 +1,26 @@
-print(3+3)
+"""
+Python Code Challenge #1: Find Prime Factors
+Your goal is to implement a function, get_prime_factors(), that takes an integer value as the input argument and returns a list containing all of its prime factors.
+
+Example Test Output
+>>> get_prime_factors(630)
+[2, 3, 3, 5, 7]
+
+"""
+
+
+def get_prime_factors(number):
+    list_factor = []
+    divisor = 2
+    while divisor <= number:
+        if number % divisor == 0:
+            list_factor.append(divisor)
+            number /= divisor
+        else:
+            divisor += 1
+    return list_factor
+
+print(get_prime_factors(630))
+print(get_prime_factors(1330))    
 
 
